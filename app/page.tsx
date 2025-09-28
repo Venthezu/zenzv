5a7522d0483e7f90c0bd9c6c2833f72c407ff38f
Zuu (ZVHive Own) ❔ <shinzcorporation@gmail.com>
2025-09-29 01:31:30 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Uploader from '../components/Uploader';
+
+export default function Home() {
+  return (
+    <div className="mt-10">
+      <h1 className="text-4xl font-bold mb-6 text-center">ZenZv Upload</h1>
+      <Uploader />
+    </div>
+  );
+}
